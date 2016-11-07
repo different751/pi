cbdb013571ba756009ca1a7bad9b70d849d83d6e
tyler.coy <tyler.coy@wsu.edu>
2016-11-06 21:50:00 -0800

    Completed program 	-also includes a readme

--- README.txt
+++ README.txt
@@ -1,2 +1,35 @@
-Name:
-Email:
+Name:Tyler Coy
+Email:tyler.coy@wsu.edu
+
+Description:
+
+This program is designed to find
+the estimate of pi by using
+four different types of implementations
+for apporximating definite integerals.
+The program uses the trapizoid rule,
+simpsons 1/3 rule, simpsons 3/8 rule, and
+booles rule. Once the program is started
+values will start to be given to each
+of the definite integral approximations.
+After the calculations have been complete
+a table of the errors compared to the real
+value of pi are printed to standard out.
+
+How to compile:
+
+To build all programs in the archive the user should type
+"make" in the command line. Typing "make" will run a script
+which will compile all files in the archive. If the user
+wioshes to run the pi.c program tey chould type ./pi into the
+command lkine after building the program with "make". If the
+user wishes to test all files in the archive they should type
+"prove" into the command line.
+
+Files in archive:
+
+Makefile
+pi.c
+README.txt
+t
+
--- pi.c
+++ pi.c
@@ -1,3 +1,12 @@
+/*
+
+Name: Tyler Coy
+Date: 11/6/16
+Programming Assignment: Pi aproximations
+Class: CS330
+*/
+
+
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
@@ -6,14 +15,14 @@
 #define M_PI 3.141562953589793238462643383279502884
 #endif
 
-static long double trapezoid(long double (*f)(long double), 
+static long double trapezoid(long double (*f)(long double),
                              long double a, long double b, int n);
-static long double simpsons_third(long double (*f)(long double), 
+static long double simpsons_third(long double (*f)(long double),
                                   long double a, long double b, int n);
-static long double simpsons_three_eighth(long double (*f)(long double), 
-                                         long double a, long double b, 
+static long double simpsons_three_eighth(long double (*f)(long double),
+                                         long double a, long double b,
                                          int n);
-static long double boole(long double (*f)(long double), 
+static long double boole(long double (*f)(long double),
                          long double a, long double b, int n);
 static long double f(long double x);
 
@@ -22,45 +31,156 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Usage: %s\n", argv[0]);
     return 1;
   }
-  
+
   const int maxn = 800000;
-  printf("%8s %-16s %-16s %-16s %-16s\n", 
-         "N", "Trapezoid", "Simpson1/3", "Simpson3/8", "Boole");
+  printf("%8s %-16s %-16s %-16s %-16s\n",
+         "N", "Trapezoid", "Simpson1/3", "Simpson3/8", "Boole");//print table headings
   for (int n = 12; n <= maxn; n *= 2) {
-    printf("%8d %0.10Le %0.10Le %0.10Le %0.10Le\n",
+    long double trap = fabsl(M_PI - trapezoid(f, 0, 1, n));
+    printf("%8d %0.10Le %0.10Le %0.10Le %0.10Le\n",//print values
            n,
-           fabsl(M_PI - trapezoid(f, 0, 1, n)),
+           trap,
            fabsl(M_PI - simpsons_third(f, 0, 1, n)),
            fabsl(M_PI - simpsons_three_eighth(f, 0, 1, n)),
            fabsl(M_PI - boole(f, 0, 1, n)));
+
+
   }
   return 0;
 }
 
 static long double f(long double x) {
-  return 4.0L / (1.0L + x*x);
+  return 4.0L / (1.0L + x*x); //return value for f(x)
 }
 
-static long double trapezoid(long double (*f)(long double), 
+
+
+
+static long double trapezoid(long double (*f)(long double),
                              long double a, long double b, int n) {
-  return 0.0L;
+
+
+        assert(a<b);    //check if a is smaller then b in order to continue
+        const double h = (b-a)/n; //initialize h value
+        double sum = 0.0;
+
+        for (int i = 1; i<n; i++) //for loop for the sum of f(x)s
+                sum += f(a+h*i);
+        return h*((f(a) + f(b))/2 + sum); //return the formula for the trapizoid rule
+
+
+
+
+
+
 }
 
-static long double simpsons_third(long double (*f)(long double), 
+
+
+
+
+static long double simpsons_third(long double (*f)(long double),
                                   long double a, long double b, int n) {
-  assert((n % 2) == 0); // n must be multiple of 2
-  return 0.0L;
+  assert(n % 2 == 0); // n must be multiple of 2
+
+
+        long double h = (b - a)/n; //initialize h value
+        long double h2=h+h; //initialize h2 to fix rounding errors
+        long double x=a+h; //initialize x to fix rounding errors
+long double sum1 = 0.0;
+        for (int i = 1; i < n; i += 2){  //summation for the first f(x)
+                sum1 += f(x);
+                x+=h2;
+
+        }
+long double sum2 = 0.0;
+x=a+h+h; //reset x value
+for (int d = 2; d < n; d += 2){ //second summation for f(x)
+sum2 += f(x);
+x+=h2;
+
+}
+return (h/3)*((f(a) + f(b)) + 4*sum1 + 2*sum2); //return the vaule
 }
 
-static long double simpsons_three_eighth(long double (*f)(long double), 
-                                         long double a, long double b, 
+
+
+
+static long double simpsons_three_eighth(long double (*f)(long double),
+                                         long double a, long double b,
                                          int n) {
   assert((n % 3) == 0); // n must be multiple of 3
-  return 0.0L;
+
+        long double h = (b - a)/(n);  //initalize h value
+        long double x = a + h;  //intialize x value to fix rounding errors
+        long double sum1=0.0;
+        long double sum3=0.0;
+
+        long double h3 = h+h+h;
+        for(int i=1;i<n;i+=3){  //summation for both f(x) and f(x+h)
+
+                //sum1+=f(a+h*i);
+                sum1+=f(x)+f(x+h);
+                //sum2+=f(x+h);
+                x+=h3;
+
+        }
+
+
+        x=a+h+h+h; //reset x value
+        for(int v=3;v<n;v+=3){  //summation for next f(x)
+
+                //sum3+=f(a+h*v);
+                sum3+=f(x);
+                x+=h3;
+
+        }
+
+
+        return(((3*h)/8)*((f(a)+f(b)) + 3*(sum1) + 2 * sum3));  // return value
+
 }
 
-static long double boole(long double (*f)(long double), 
+
+
+
+
+static long double boole(long double (*f)(long double),
                          long double a, long double b, int n) {
   assert((n % 4) == 0); // n must be multiple of 4
-  return 0.0L;
+
+
+        long double h =(b-a)/(n);  //initialize h value
+        long double sum1=0.0;
+        long double sum2=0.0;
+        long double sum3=0.0;
+
+        for(int i=1;i<n;i+=2){  //summation for the first f(x)
+
+        sum1+=f(a+h*i);
+
+        }
+
+
+        for(int v=2;v<n;v+=4){  //summation for the second f(x)
+
+        sum2+=f(a+h*v);
+
+        }
+
+
+        for(int c=4;c<n;c+=4){  //summation for the thrid f(x)
+
+        sum3+=f(a+h*c);
+
+
+        }
+
+
+
+        return ((2*h)/45)*(7*(f(a)+f(b)) + (14 * sum3) + (32 * sum1) + (12 * sum2));  //return value
+
+
+
+
 }
